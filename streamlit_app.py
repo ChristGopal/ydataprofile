e6b9e555c08e610e4fda0501dffbfa0d7dcf247f
Gopal <73210658+ChristGopal@users.noreply.github.com>
2024-11-08 12:08:56 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,12 @@
 import numpy as np
 import pandas as pd
-# from ydata_profiling import ProfileReport
+from ydata_profiling import ProfileReport
 import streamlit as st
 from streamlit_ydata_profiling import st_profile_report
 
 df = pd.read_csv('Entegris_PowerCons.csv')
-pr = st_profile_report(df)
-st_profile_report(pr, navbar=True)
+pr = ProfileReport(df)
+ProfileReport(pr, navbar=True)
 
 st.title("Power Consumption Y Data Profile")
 st.write(
