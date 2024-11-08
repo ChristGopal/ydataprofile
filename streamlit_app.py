e1f5019efe76ba8bc8c99c9b3fa317a515951368
Gopal <73210658+ChristGopal@users.noreply.github.com>
2024-11-08 11:51:47 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,14 @@
+import numpy as np
+import pandas as pd
+from ydata_profiling import ProfileReport
 import streamlit as st
+from streamlit_ydata_profiling import st_profile_report
 
-st.title("🎈 My new app")
+df = pd.read_csv('air_comp_data_new.csv')
+pr = ProfileReport(df, minimal=True, orange_mode=True, explorative=True)
+st_profile_report(pr, navbar=True)
+
+st.title("Power Consumption Y Data Profile")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
